764690d6fbe1e1baa9702e728f2f14d576f14c27
pengda <3053787951@qq.com>
2023-10-16 18:22:47 +0800

    chore: 添加install_pro.sh 脚本

---
+++ install_pro.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+code_dir=$(basename `pwd`)
+cd ..
+
+dir=$(mktemp -d)
+cp $code_dir/* $dir -r
+cd $dir
+rm .git -rf
+rm .idea -rf
+rm go.mod go.sum -f
+rm LICENSE -f
+rm README.md -f
+rm install-pro.sh
+
+find -iname '*.go' -exec sed -i 's@github.com/beeker1121/goque@npd/pkg/goque@' {} \;
+
+rm -rf test_queue ack-design.txt
+
+cd ..
+dst=~/pro_code/npd/pkg/goque
+mkdir -p $dst
+cp $dir/* $dst -r
+
+rm $dir -rf
